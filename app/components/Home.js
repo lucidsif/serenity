e409da35717aa22418a99df99b7221ec0d6ec6eb
Tawsif Ahmed <ahmedt93@gmail.com>
2017-09-24 18:46:21 -0400

    added a test fs.readfile on componentdidmount

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,16 +1,30 @@
 // @flow
 import React, { Component } from 'react';
 import styles from './Home.css';
+import fs from 'fs';
 
 export default class Home extends Component {
+
+  componentDidMount() {
+    const desiredFile = process.cwd() + '/README.md';
+    console.log('desired file: ', desiredFile)
+    fs.readFile(desiredFile, 'utf8', (err, file) => {
+      if (err) {
+        console.log('error!!!', err);
+      } else {
+        console.log('file read', file.slice(0, 100));
+      }
+    });
+  }
+
   render() {
-    console.log('bci', this.props.bci)
-    const bci = this.props.bci
+    console.log('bci', this.props.bci);
+    const bci = this.props.bci;
     return (
       <div>
         <div className={styles.container} data-tid="container">
           {
-            bci.connected ? <h3 style={{color: 'green'}}>Connected</h3> : <h3 style={{color: 'red'}}>Disconnected</h3>
+            bci.connected ? <h3 style={{ color: 'green' }}>Connected</h3> : <h3 style={{ color: 'red' }}>Disconnected</h3>
           }
           <div>
           Signal Strength: 0
