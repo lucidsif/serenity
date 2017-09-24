21bc06007661a363ca9ae2e77ed55ded520897a7
Tawsif Ahmed <ahmedt93@gmail.com>
2017-09-24 18:17:18 -0400

    wrote static content on home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import styles from './Home.css';
 
 export default class Home extends Component {
@@ -8,8 +7,19 @@ export default class Home extends Component {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <h2>Disconnected</h2>
+          <div>
+          Signal Strength: 0
+          </div>
+          <div>
+          Battery Level: 0
+          </div>
+          <div>
+          Relaxation Level: 0
+          </div>
+          <div>
+          Stress Level: 0
+          </div>
         </div>
       </div>
     );
